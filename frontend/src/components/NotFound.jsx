b9237689010e9dbe6cf8cb9fa869b947309828aa
MichaelBarnor <michael.77k@gmail.com>
2025-08-04 19:55:24 -0400

    fixed default function error with notfound

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,4 +1,4 @@
-export default function NotFound() {
+export function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
       <img
