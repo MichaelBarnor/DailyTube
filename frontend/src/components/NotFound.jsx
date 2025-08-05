da70064a2e8c274e0d81362300ecbca226495ce0
MichaelBarnor <michael.77k@gmail.com>
2025-08-04 20:04:50 -0400

    changed styling of not found componet

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,18 +1,13 @@
 export function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <img
-        src="/tabicon.png"
-        alt="DailyTube Logo"
-        className="w-16 h-16 mb-6"
-      />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[#161a23] text-white">
       <h1 className="text-4xl font-bold mb-2">404 - Page Not Found</h1>
       <p className="mb-6 text-lg text-gray-300">
         Sorry, the page you’re looking for doesn’t exist.
       </p>
       <a
         href="/"
-        className="px-6 py-2 bg-blue-600 hover:bg-blue-700 rounded text-white font-semibold transition"
+        className="px-6 py-2 bg-red-600 hover:bg-red-700 rounded-full text-white font-semibold transition"
       >
         Go Home
       </a>
